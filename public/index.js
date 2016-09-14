03294288123407c0c7418627b2c031e44995a035
Patrick Roumanoff <patrick.roumanoff@iag.com>
2016-09-14 22:08:12 +0200

    Images on First Page

--- public/index.js
+++ public/index.js
@@ -1,59 +1,67 @@
+var loggedIn = false;
+
 firebase.auth().onAuthStateChanged(function(user) {
-  if (user) {
-    // User is signed in.
-    var isAnonymous = user.isAnonymous;
-    var uid = user.uid;
-    console.log("signed in");
-    // getStories();
-  } else {
-    console.log("sign out");
-  }
+    if (user && !loggedIn) {
+        console.log("signed in:", user.uid);
+        loggedIn = true;
+        getStories();
+    } else if (!user) {
+        console.log("signed out");
+        loggedIn = false;
+    }
 });
 
 firebase.auth().signInAnonymously().catch(function(error) {
-  // Handle Errors here.
-  var errorCode = error.code;
-  var errorMessage = error.message;
-  console.log(errorCode, errorMessage, error);
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    console.log(errorCode, errorMessage, error);
 });
 
 function getStories() {
-  var stories = firebase.database().ref("/stories").once("value").then(function (d) {
-    var i;
-    for(i in Object.keys(d.val())) {
-      console.log(i);
-    }
-  });
+    var images = document.querySelectorAll("img");
+    var stories = firebase.database().ref("/stories").once("value").then(function(d) {
+        var i, j = 0;
+        for (i in d.val()) {
+            var imageRef = d.val()[i].imageRef;
+            if(imageRef) {
+              console.log(i, d.val()[i]);
+              firebase.storage().ref().child(imageRef).getDownloadURL().then(function(url) {
+                images.item(j++).src = url;
+              }).catch(function(error) {
+                  console.log(i, imageRef, error);  
+              });
+            }
+        }
+    });
 }
 
 function ready() {
-  var b = document.querySelector("#share");
-  if(b) {
-    b.onclick = function (e) {
-      var storyData = {
-        "imNot": document.querySelector("#notjust").value,
-        "imAlso": document.querySelector("#also").value,
-        "story": document.querySelector("#story").value
-      };
+    var b = document.querySelector("#share");
+    if (b) {
+        b.onclick = function(e) {
+            var fileInput = document.querySelector("#f");
+            var file = fileInput.files[0];
 
-      var f = document.querySelector("#f");
-      var file = f.files[0];
-      console.log(file);
-      var filename = file.name;
-      var storageRef = firebase.storage().ref();
-      var imageRef = storageRef.child('images/' + filename);
-      imageRef.put(file).then(function(snapshot) {
-        console.log('Uploaded a file!');
-      });
-      var newPostKey = firebase.database().ref().child('stories').push().key;
-      var key = '/stories/' + newPostKey;
-      var updates = {};
-      updates[key] = storyData;
+            var newPostKey = firebase.database().ref().child('stories').push().key;
+            var storyData = {
+                imNot: document.querySelector("#notjust").value,
+                imAlso: document.querySelector("#also").value,
+                story: document.querySelector("#story").value,
+                imageRef: "images/" + newPostKey + "/" + file.name
+            };
 
-      firebase.database().ref().update(updates);
-      return false;
-    };
-  }
+            firebase.storage().ref().child(storyData.imageRef).put(file).then(function(snapshot) {
+                console.log('Uploaded a file under ' + storyData.imageRef);
+            });
+
+            var key = '/stories/' + newPostKey;
+            var updates = {};
+            updates[key] = storyData;
+            firebase.database().ref().update(updates);
+            return false;
+        };
+    }
 }
 
 window.onload = ready;
